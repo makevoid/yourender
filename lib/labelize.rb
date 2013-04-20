194130954df911820c946c586d28e44ca4908dce
Francesco Canessa <makevoid@gmail.com>
2013-04-20 22:25:03 +0200

    strip and organize

--- lib/labelize.rb
+++ lib/labelize.rb
@@ -3,13 +3,15 @@ module Labelize
   LABELS = {
     aws_register:       "Registra un account AWS",
     create_ami:         "Crea un'immagine server (AMI)",
-    install_rdp_osx:    "Installa RDP (Mac)",
     connect_osx:        "Connettiti al server (Mac)",
+    install_rdp_osx:    "Installa RDP (Mac)",
     connect_win:        "Connettiti al server (Windows)",
     install_rdp_win:    "Installa RDP (Windows)",
     launch_ami:         "Lancia un server dall'immagine",
     terminate_instance: "Termina il server",
     install_software:   "Installa i software",
+    everyday_use:       "Utilizzo Standard",
+    check_usage:        "Controllo delle spese",
   }
 
   def label(name)
